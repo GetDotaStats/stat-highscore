84a0a244c294849dcac9165159cee2ce2ca19d5e
Trent Monahan <github@361zn.is>
2018-01-29 07:36:47 +1100

    Updated Lua Highscores to current spec, added highscoreLobby as a helper function

--- game/scripts/vscripts/statcollection/highscores.lua
+++ game/scripts/vscripts/statcollection/highscores.lua
@@ -53,6 +53,15 @@ function statCollection:highscoreTop(highscoreID, callback)
         callback(res)
     end)
 end
+function statCollection:highscoreLobby(highscoreID, callback)
+    local players = {}
+    for playerID = 0, DOTA_MAX_TEAM_PLAYERS do
+        if PlayerResource:IsValidPlayerID(playerID) then
+            table.insert(players, playerID)
+        end
+    end
+    statCollection:highscoreList(highscoreID, players, callback)
+end
 function statCollection:highscoreList(highscoreID, pid, callback)
     -- If we are missing required parameters, then don't send
     if not self.doneInit then
@@ -63,9 +72,19 @@ function statCollection:highscoreList(highscoreID, pid, callback)
         type = "LIST",
         modIdentifier = self.modIdentifier,
         highscoreID = highscoreID,
-        steamID32 = PlayerResource:GetSteamAccountID(pid),
         schemaVersion = statCollection.highscoreSchemaVersion
     }
+    if type(pid) == "number" then
+        payload.steamID32 = {PlayerResource:GetSteamAccountID(pid)}
+    else
+        payload.steamID32 = {}
+        print(pid)
+        print(type(pid))
+        DeepPrintTable(pid)
+        for _,v in pairs(pid) do
+            table.insert(payload.steamID32, PlayerResource:GetSteamAccountID(v))
+        end
+    end
     statCollection:sendStage("s2_highscore.php", payload, function(err, res)
         -- Check if we got an error
         if statCollection:ReturnedErrors(err, res) then
